c9fe87fe238b1ed8c61d59d823899cf1ec1db7f7
Antonio Bennett <bennettantonio199@gmail.com>
2021-06-23 11:12:24 -0400

    now taking game input and updating board state

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,12 @@
-use std::{fmt, io};
-
-enum PlayerOrder {
-    One,
-    Two,
-}
+use std::{char, fmt, io};
 
 pub struct Player {
     pub name: String,
-    order: PlayerOrder,
+    pub order: u8,
 }
 
 impl Player {
-    fn new(name: String, num: u8) -> Self {
-        let order;
-        if num == 1 {
-            order = PlayerOrder::One;
-        } else {
-            order = PlayerOrder::Two;
-        }
+    fn new(name: String, order: u8) -> Self {
         Self {
             name: name.to_string(),
             order,
@@ -31,7 +20,19 @@ pub struct Board {
 
 impl Board {
     pub fn new() -> Self {
-        Self { board: ['\0'; 9] }
+        let mut board = ['\0'; 9];
+        for num in 1..=9 {
+            board[num - 1] = char::from_digit(num as u32, 10).unwrap();
+        }
+        Self { board }
+    }
+
+    pub fn check_game(&self) -> bool {
+        todo!()
+    }
+
+    pub fn modify_board(&mut self, pos: usize, character: char) {
+        self.board[pos - 1] = character;
     }
 }
 
@@ -53,7 +54,7 @@ impl fmt::Display for Board {
     }
 }
 
-pub fn menu_input() -> (Player, Player) {
+pub fn game_setup() -> (Player, Player) {
     let mut player1 = String::new();
     let mut player2 = String::new();
 
@@ -67,15 +68,36 @@ pub fn menu_input() -> (Player, Player) {
         .read_line(&mut player2)
         .expect("Could not read player 2 name");
 
-    let player1 = player1.trim();
-    let player2 = player2.trim();
+    let player1 = player1.trim().to_owned();
+    let player2 = player2.trim().to_owned();
 
-    let player1 = Player::new(player1.to_owned(), 1);
-    let player2 = Player::new(player2.to_owned(), 2);
+    let player1 = Player::new(player1, 1);
+    let player2 = Player::new(player2, 2);
 
     (player1, player2)
 }
 
-pub fn input(_player: Player) -> Player {
-    Player::new("test".to_owned(), 1)
+pub fn game_input(board: &mut Board, player: &Player, game_over: &mut bool) {
+    match board.check_game() {
+        false => {
+            println!("{} choose your move: [1-9]", player.name);
+            let mut position = String::new();
+            io::stdin()
+                .read_line(&mut position)
+                .expect("Couldn't read position choice");
+
+            let position = position.trim().parse::<usize>().unwrap();
+            let character: char;
+
+            if player.order == 1 {
+                character = 'X';
+            } else {
+                character = 'O';
+            }
+            board.modify_board(position, character);
+        }
+        true => {
+            *game_over = true;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use tic_tac_toe::*;
 
 fn main() {
-    let (player1, player2) = menu_input();
+    let (player1, player2) = game_setup();
     println!(
         "Player 1 is {} and Player 2 is {}",
         player1.name, player2.name
     );
 
-    let board = Board::new();
-    println!("{}", board);
+    let mut board = Board::new();
+
+    let mut game_over = false;
+    let mut curr_player = &player1;
+
+    while !game_over {
+        println!("{}", board);
+        game_input(&mut board, &curr_player, &mut game_over);
+        if curr_player.order == 1 {
+            curr_player = &player2;
+        } else {
+            curr_player = &player1;
+        }
+    }
 }
